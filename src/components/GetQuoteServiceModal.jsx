de186063cd3cd57489cd0b7db1bde560b30a4784
rajadurainataraj <117814134+rajadurainataraj@users.noreply.github.com>
2024-04-05 18:48:19 +0530

    updated modal

--- src/components/GetQuoteServiceModal.jsx
+++ src/components/GetQuoteServiceModal.jsx
@@ -5,17 +5,17 @@ const GetQuoteServiceModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
+    console.log("hi");
     setIsModalOpen(true);
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
   };
-
+  console.log(isModalOpen);
   return (
     <div className="container-fluid">
       <button onClick={openModal}>Open Modal</button>
-      <CustomModal isOpen={isModalOpen} onClose={closeModal} />
     </div>
   );
 };
