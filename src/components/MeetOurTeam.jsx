57df7642ee932612d56b2cbc285ad552f6fed844
rajadurainataraj <rajadurainatraj@gmail.com>
2024-05-13 23:48:35 +0530

    meet our team fixed

--- src/components/MeetOurTeam.jsx
+++ src/components/MeetOurTeam.jsx
@@ -4,11 +4,11 @@ import { team } from './datas'
 
 const MeetOurTeam = () => {
   return (
-    <section className="py-4  container-fluid meet-our-team">
+    <section className="py-4 container-fluid meet-our-team">
       <h3 className="content-header text-center pt-5">Meet Our Team</h3>
       <div className="scroll-container pt-3">
         <div
-          className="d-flex custom-cards gap-5 justify-content-center align-item-center"
+          className="d-flex custom-cards gap-5 "
           style={{ marginRight: '20px' }}
         >
           {team.map((profile) => (
