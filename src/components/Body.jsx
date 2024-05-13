f3fef8ebd23ceb4318779008ecb4b43e48d5973d
rajadurainataraj <rajadurainatraj@gmail.com>
2024-05-13 23:43:28 +0530

    updated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,29 +1,29 @@
 /* eslint-disable no-unused-vars */
-import Hero from "./Hero";
-import AboutUs from "./AboutUs";
-import WhatWeDo from "./WhatWeDo";
-import Digital from "./Digital";
-import MeetOurTeam from "./MeetOurTeam";
-import OurClients from "./OurClients";
+import Hero from './Hero'
+import AboutUs from './AboutUs'
+import WhatWeDo from './WhatWeDo'
+import Digital from './Digital'
+import MeetOurTeam from './MeetOurTeam'
+import OurClients from './OurClients'
 
 // import Slider from "./Slider";
 
-import Card from "./Card";
-import ContactInfo from "./ContactInfo";
-import Technologies2 from "./Technologies2";
+import Card from './Card'
+import ContactInfo from './ContactInfo'
+import Technologies2 from './Technologies2'
 // import CarouselSwip from "./CarouselSwip";
-import Footer from "./footer/Footer";
-import GetQuote from "./GetQuote";
-import CompanyReview from "./CompanyReview";
-import { useRecoilState } from "recoil";
-import { count } from "./utils/globalState";
+import Footer from './footer/Footer'
+import GetQuote from './GetQuote'
+import CompanyReview from './CompanyReview'
+import { useRecoilState } from 'recoil'
+import { count } from './utils/globalState'
 const Body = () => {
-  const [counts, setCounts] = useRecoilState(count);
+  const [counts, setCounts] = useRecoilState(count)
   return (
-    <div className={`custom-main-div ${counts !== 0 && "bg-light"}`}>
+    <div className={`custom-main-div ${counts !== 0 && 'bg-light'}`}>
       <Hero />
       <AboutUs />
-      <WhatWeDo />
+      {/* <WhatWeDo /> */}
       <Digital />
       <MeetOurTeam />
       <OurClients />
@@ -31,7 +31,7 @@ const Body = () => {
       <Technologies2 />
       <GetQuote />
     </div>
-  );
-};
+  )
+}
 
-export default Body;
+export default Body
